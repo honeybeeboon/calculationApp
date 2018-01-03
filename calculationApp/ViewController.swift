729e226e771e1c6f9b618834e2e0e4d790ebd4f6
kazuki <firstshining.flowerflower.yui@gmail.com>
2018-01-03 14:58:05 +0900

    Initial Commit

---
+++ calculationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculationApp
+//
+//  Created by 下澤一輝 on 2018/01/03.
+//  Copyright © 2018年 下澤一輝. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
