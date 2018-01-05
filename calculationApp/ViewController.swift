47f774feb206e0fe88fcdc497d77606297e2e854
kazuki <firstshining.flowerflower.yui@gmail.com>
2018-01-05 10:25:49 +0900

    display num label

--- calculationApp/ViewController.swift
+++ calculationApp/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func inputNum(_ sender: UIButton) {
+        resultLabel.text = sender.titleLabel!.text
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
